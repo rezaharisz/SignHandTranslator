c20aa652650e49d9d571d003596b9ee0eb30dfce
Your Name <you@example.com>
2021-08-11 02:42:26 +0700

    first commit

---
+++ app/src/main/java/com/alfikri/signhandtranslator/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.alfikri.signhandtranslator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
