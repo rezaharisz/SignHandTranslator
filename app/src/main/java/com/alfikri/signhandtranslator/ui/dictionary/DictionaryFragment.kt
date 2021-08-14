2aeb43ba38f7eae555ff3e481613315d766e0ea6
Your Name <you@example.com>
2021-08-14 11:47:02 +0700

    update and some adjustment

---
+++ app/src/main/java/com/alfikri/signhandtranslator/ui/dictionary/DictionaryFragment.kt
@@ -0,0 +1,17 @@
+package com.alfikri.signhandtranslator.ui.dictionary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alfikri.signhandtranslator.R
+
+class DictionaryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dictionary, container, false)
+    }
+
+}
