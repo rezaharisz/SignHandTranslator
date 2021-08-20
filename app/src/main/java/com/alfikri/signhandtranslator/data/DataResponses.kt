bd4a108f936e8f172a961e53c283448b02c8b121
Your Name <you@example.com>
2021-08-20 21:16:39 +0700

    sign up firebase implementation and some update

---
+++ app/src/main/java/com/alfikri/signhandtranslator/data/DataResponses.kt
@@ -0,0 +1,13 @@
+package com.alfikri.signhandtranslator.data
+
+data class DataResponses(
+    val email: String? = null,
+    val password: String? = null,
+    val imgProfile: String? = null,
+    val name: String? = null,
+    val username: String? = null,
+    val phoneNumber: String? = null,
+    val city: String? = null,
+    val gender: String? = null,
+    val aboutMe: String? = null
+)
--- app/src/main/java/com/alfikri/signhandtranslator/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/alfikri/signhandtranslator/ui/register/RegisterFragment.kt
@@ -9,11 +9,17 @@ import android.widget.Toast
 import androidx.navigation.findNavController
 import com.alfikri.signhandtranslator.R
 import com.alfikri.signhandtranslator.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var databaseReference: DatabaseReference? = null
+    private var firebaseDatabase: FirebaseDatabase? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
@@ -30,15 +36,48 @@ class RegisterFragment : Fragment() {
 
         binding.btnRegister.setOnClickListener {
             //DO REGISTER ACTION WITH FIREBASE
+            when{
+                binding.edFullname.text.toString().isEmpty() -> {
+                    binding.edFullname.error = "Please enter your name"
+                }
+                binding.edEmail.text.toString().isEmpty() -> {
+                    binding.edEmail.error = "Please enter your email"
+                }
+                binding.edPassword.text.toString().isEmpty() -> {
+                    binding.edPassword.error = "Please enter your password"
+                }
+                else -> {
+                    callFirebase()
+                }
+            }
 
-            Toast.makeText(context, "Register Successfull, Please Login First", Toast.LENGTH_SHORT).show()
-
-            view.findNavController().navigate(R.id.action_register_to_login)
         }
 
         binding.tvAlreadymember.setOnClickListener {
             view.findNavController().navigate(R.id.action_register_to_login)
         }
+    }
+
+    private fun callFirebase(){
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase?.reference?.child("PROFILE")
+
+        firebaseAuth.createUserWithEmailAndPassword(binding.edEmail.text.toString(), binding.edPassword.text.toString())
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    val currentUser = firebaseAuth.currentUser
+                    val userDb = databaseReference?.child(currentUser?.uid.toString())
+
+                    userDb?.child("name")?.setValue(binding.edFullname.text.toString())
+
+                    Toast.makeText(context, "Register Successfull, Please Login First", Toast.LENGTH_SHORT).show()
+
+                    view?.findNavController()?.navigate(R.id.action_register_to_login)
+                } else{
+                    Toast.makeText(context, "Registration Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 }
